921790b770ca6c845118e1e7daabcc20072de8be
Artem <kypidon71@gmail..com>
2022-11-21 23:55:20 +0300

    ADD GOODZADANIE

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+string[] array = new string[7] {"123", "23", "hello", "world", "res", "uio", "123456"};
+string[] array2 = new string[array.Length];
+void ArrayPerebor(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayPerebor(array, array2);
+PrintArray(array2);
